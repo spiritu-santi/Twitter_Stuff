f243d7cccbbafd137ac64d5806a3561ecc591d80
Santiago Ramírez Barahona <44642626+spiritu-santi@users.noreply.github.com>
2022-11-18 11:26:58 -0600

    Update draw_HPD.R

--- Draw_HPD/draw_HPD.R
+++ Draw_HPD/draw_HPD.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(magrittr)
 library(ggtree)
 tt <- treeio::read.beast("~/Desktop/MCC_trees_V1 4/SD_MCC.v_1.tre")
+# Here I am selecting nodes with an age (height) geater than 200 Mya.
+# More complex sampling of nodes can be performed.
 tt@data %>% filter(height > 200) %>% pull(node) -> root_nodes
 tt@data %<>% #unnest(height_0.95_HPD) %>% 
   mutate(height_0.95_HPD = ifelse(node %in% root_nodes, height_0.95_HPD,NA))
